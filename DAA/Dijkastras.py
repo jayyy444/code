7d40744b9f6f7c80102aed7f9ef782f7f6b5d363
jayyy444 <jayc31665@gmail.com>
2025-06-23 17:14:22 +0530

    Update Dijkastras.py

--- DAA/Dijkastras.py
+++ DAA/Dijkastras.py
@@ -1,11 +1,11 @@
 graph = {
-    'A':[('B',1),('C',4)], # ('B',1) is tuple of list. 
+    'A':[('B',1),('C',4)], 
     'B':[('A',1),('C',2),('D',5)],
     'C':[('A',4),('B',2),('D',1)],
     'D':[('B',5),('C',1)]
 }
 source = 'A'
-distances = {node: float('inf') for node in graph} # node is dictionary
+distances = {node: float('inf') for node in graph} 
 distances[source] = 0
 visited = set()
 
